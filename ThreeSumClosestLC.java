494830384633bde9e53c1b1756595c8428b2360a
dhrruvchotai <23010101051@darshan.ac.in>
2025-07-05 21:41:41 +0530

    added closest 3 sum lc

---
+++ ThreeSumClosestLC.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ThreeSumClosestLC {
+    public static void main(String[] args) {
+        int arr[] = {-1,2,1,-4};
+        int target = 1;
+        Arrays.sort(arr);
+        int closest_sum = arr[0] + arr[1] + arr[2];
+        
+    }
+}
