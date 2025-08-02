9d91c0651a6dc8fca51c523c7a9a779e5b5f3c07
dhrruvchotai <23010101051@darshan.ac.in>
2025-08-02 18:40:07 +0530

    added Lc ques

--- LC394.java
+++ LC394.java
@@ -4,19 +4,31 @@ public class LC394 {
         String str = "3[a2[c]]";
         Stack<Integer> numStack = new Stack<>();
         Stack<String> stringStack = new Stack<>();
-        int index = 0;
+        String currentStr = "";
 
-        while(index < str.length()){
-            if(Character.isDigit(str.charAt(index))){
-                numStack.push(Integer.parseInt(str.charAt(index) + ""));
+         int multiplier = 0;
+        for(char ch : str.toCharArray()){
+            if(Character.isDigit(ch)){
+                // multiplier = ch - '0';//works for only single digit
+                multiplier = multiplier*10 + (ch - '0');//for this case 100[Dhruv]
+                //  we have to use this
             }
-            else if(Character.isLetter(str.charAt(index)) || (str.charAt(index) + "") == "]"){
-                stringStack.push(str.charAt(index) + " ");
+            else if(ch == '['){
+                numStack.push(multiplier);
+                stringStack.push(currentStr);
+                multiplier = 0;
+                currentStr = "";
             }
-            index++;
+            else if(ch == ']'){
+                int repeat = numStack.pop();
+                String previous = stringStack.pop();
+                currentStr = previous + currentStr.repeat(repeat);
+            }
+            else{
+                currentStr += ch;
+            }
         }
 
-        System.out.println(stringStack);
-
+        System.out.println(currentStr);
     }
 }
